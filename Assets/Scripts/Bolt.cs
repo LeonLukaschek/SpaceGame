19578fd3438acdebd19a165b6cc00b5e2bfd15e9
Leon Lukaschek <leon.lukaschek@gmail.com>
2016-01-08 20:25:06 +0100

    Init

---
+++ Assets/Scripts/Bolt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float speed;
+
+    private void Update()
+    {
+        Vector3 move = new Vector3(0, speed * Time.deltaTime, 0);
+        this.transform.position += move;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject, 1f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed;
+
+    [Space(15)]
+    [Header("Shooting")]
+    public float timeBetweenShots;
+
+    public GameObject shotSpawnPoint;
+    public GameObject bolt;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        //Moving
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+
+        //Shooting
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bolt, shotSpawnPoint.transform.position, transform.rotation);
+    }
+}
